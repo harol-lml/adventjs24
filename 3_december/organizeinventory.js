f5c57b7f5141dc48fc4ea35e6ef589bc3c338521
harol-lml <hapr0023@gmail.com>
2024-12-05 15:22:15 -0500

    descriptions

--- 3_december/organizeinventory.js
+++ 3_december/organizeinventory.js
@@ -1,18 +1,3 @@
-/*
-    Santa Claus 🎅 is checking his workshop inventory to prepare gift delivery. The elves have recorded the toys in an array of objects, but the information is a bit disorganized. You need to help Santa organize the inventory.
-
-    You will receive an array of objects, where each object represents a toy and has the properties:
-
-    name: the name of the toy (string).
-    quantity: the available quantity of that toy (integer).
-    category: the category to which the toy belongs (string).
-    Write a function that processes this array and returns an object that organizes the toys as follows:
-
-    The keys of the object will be the categories of toys.
-    The values will be objects that have the toy names as keys and the total quantities of each toy in that category as values.
-    If there are toys with the same name in the same category, you must sum their quantities.
-    If the array is empty, the function should return an empty object {}.
-*/
 function organizeInventory(inventory) {
     return inventory.reduce((acc, { name, quantity, category }) => {
         acc[category] ??= {}; // Inicializa la categoría si no existe
--- 4_december/treeornament.js
+++ 4_december/treeornament.js
@@ -1,15 +1,3 @@
-/*
-    It's time to put up the Christmas tree at home! 🎄 But this year we want it to be special. We're going to create a function that receives the height of the tree (a positive integer between 1 and 100) and a special character to decorate it.
-
-    The function should return a string that represents the Christmas tree, constructed as follows:
-
-    The tree is made up of triangles of special characters.
-    The spaces on the sides of the tree are represented with underscores _.
-    All trees have a trunk of two lines, represented by the # character.
-    The tree should always have the same length on each side.
-    You must ensure the tree has the correct shape using line breaks \n for each line.
- */
-
 function createXmasTree(height, ornament) {
     const createRow = (spaces, content) => '_'.repeat(spaces) + content + '_'.repeat(spaces);
 
