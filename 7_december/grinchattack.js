0ae1066ed4d8e4dd7b77089de34f29c88eff91e6
harol-lml <hapr0023@gmail.com>
2024-12-07 11:53:54 -0500

    december 7

---
+++ 7_december/grinchattack.js
@@ -0,0 +1,32 @@
+/** @param {string} packages
+ *  @returns {string} Fixed and sorted packages
+ */
+function fixPackages(packages) {
+    let stack = [];
+    let result = '';
+    for (let char of packages) {
+      if (char === ')') {
+        // Si encontramos un paréntesis de cierre, resolvemos el último grupo.
+        let temp = '';
+        while (stack.length > 0 && stack[stack.length - 1] !== '(') {
+          temp = stack.pop() + temp;  // Colocamos el contenido en orden inverso.
+
+        }
+
+        stack.pop();  // Eliminamos el paréntesis de apertura '('.
+        console.log(temp);
+
+        stack.push(temp.split('').reverse().join(''));  // Añadimos el resultado invertido a la pila.
+      } else {
+        // Si no es un paréntesis, lo agregamos a la pila.
+        stack.push(char);
+      }
+    }
+    console.log(stack)
+    // Al final, unimos el contenido de la pila y lo retornamos.
+    return stack.join('');
+  }
+
+let packages = fixPackages('a(bc(def)g)h')
+
+console.log(packages)
