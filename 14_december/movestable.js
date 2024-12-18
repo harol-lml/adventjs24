2c6b412ed4633011b2fb4b166f78344711c43fc1
harol-lml <hapr0023@gmail.com>
2024-12-18 13:48:44 -0500

    december 14

---
+++ 14_december/movestable.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} reindeer
+ * @param {number[]} stables
+ * @returns {number}
+ */
+function minMovesToStables(reindeer, stables) {
+    let stalesCopy = stables.slice()
+    let rem = reindeer.map((reind) => {
+        let band = 100000, num  = 0
+
+        stalesCopy.forEach((stable, ind) => {
+            let pla = Math.abs(stable - reind)
+            if( pla < band){
+                band = pla
+                num  = ind
+            }
+        })
+        stalesCopy.splice(num, 1)
+
+        return band
+    })
+
+    return rem.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+    )
+}
+
+minMovesToStables([2, 6, 9], [3, 8, 5]) // -> 3
+// Explicación:
+// Renos en posiciones: 2, 6, 9
+// Establos en posiciones: 3, 8, 5
+// 1er reno: se mueve de la posición 2 al establo en la posición 3 (1 movimiento).
+// 2do reno: se mueve de la posición 6 al establo en la posición 5 (1 movimiento)
+// 3er reno: se mueve de la posición 9 al establo en la posición 8 (1 movimiento).
+// Total de movimientos: 1 + 1 + 1 = 3 movimientos
+
+minMovesToStables([1, 1, 3], [1, 8, 4])
+// Explicación:
+// Renos en posiciones: 1, 1, 3
+// Establos en posiciones: 1, 8, 4
+// 1er reno: no se mueve (0 movimientos)
+// 2do reno: se mueve de la posición 1 al establo en la posición 4 (3 movimientos)
+// 3er reno: se mueve de la posición 3 al establo en la posición 8 (5 movimientos)
+// Total de movimientos: 0 + 3 + 5 = 8 movimientos
